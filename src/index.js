9484b312b482bd3a5513dcededfd08cd273fdc3d
Nick Sette <njs2394@gmail.com>
2023-11-02 10:57:39 -0400

    I did it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+const taskForm = document.getElementById("create-task-form")
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let whatIamAdd = e.target.description.value
+    fillList(whatIamAdd)
+
+  })
+
+
+
+  function fillList(task) {
+    console.log(task)
+    const toDos = document.getElementById("list")
+    const li = document.createElement("li");
+    li.textContent = task;
+
+
+    toDos.append(li)
+
+  }
+
+
 });
+
+
+
